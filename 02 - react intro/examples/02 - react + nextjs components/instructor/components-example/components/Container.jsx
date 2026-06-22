0a4578675365c436015c98d7ed52f1cd5379f8a9
oa <oliver+github@abolish.golf>
2026-06-22 17:51:53 -0600

    rev [02 - react intro]: ex02 - aaand with destructuring instead

--- 02 - react intro/examples/02 - react + nextjs components/instructor/components-example/components/Container.jsx
+++ 02 - react intro/examples/02 - react + nextjs components/instructor/components-example/components/Container.jsx
@@ -1,4 +1,4 @@
-export default function Container(props) {
+export default function Container({ children }) {
   // 'children' is a hardcoded prop for anything nested inside that component when it's called.
   // It is automatically passed if you take all your props in as one argument (e.g. props -> props.children),
   // but you need to specifically name it in the function's inputs if destructuring (i.e. { children} ).
@@ -6,7 +6,7 @@ export default function Container(props) {
   // What we get here is basically an empty component that will render whatever is nested inside it when called in JSX.
   return (
     <>
-      {props.children}
+      {children}
     </>
   )
 }
