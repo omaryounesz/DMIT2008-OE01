49e33102e65c629fc37fbf55af01acdcc76ebe4d
oa <oliver+github@abolish.golf>
2026-06-22 18:42:13 -0600

    rev [02 - react intro]: ex03 - map out & render list items (don't forget key prop!)

--- 02 - react intro/examples/03 - list props/instructor/component-lists/components/SimpsonsCharacters.jsx
+++ 02 - react intro/examples/03 - list props/instructor/component-lists/components/SimpsonsCharacters.jsx
@@ -1,7 +1,26 @@
+const SIMPSON_CHARACTERS = [
+  "Homer Simpson",
+  "Bart Simpson",
+  "Marge Simpson",
+  "Mr. Burns",
+  "Lisa Simpson",
+  "Apu Nahasapeemapetilon",
+  "Sideshow Bob",
+  "Milhouse Van Houten",
+  "Ned Flanders",
+]
+
 export default function SimpsonsCharacters() {
 
   return (
-    <>
-    </>
+    <ul>
+      {
+        SIMPSON_CHARACTERS.map(
+          (name, index) => {
+            return <li key={index}>{name}</li>
+          }
+        )
+      }
+    </ul>
   )  
 }
